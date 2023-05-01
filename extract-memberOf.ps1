a9c306e4591de8f2d038ce9eeaf9a47745204334
Dat's Github <50114858+TienDatHo@users.noreply.github.com>
2023-05-01 10:41:11 +0800

    Update extract-memberOf.ps1

--- extract-memberOf.ps1
+++ extract-memberOf.ps1
@@ -1,11 +1,12 @@
 # Enter the username of the user whose groups you want to extract
 $username = Read-Host "Please enter Username:"
+$resource = Read-Host "Please enter ResourceContextServer:"
 
 # Get the user object from Active Directory
 $user = Get-ADUser $username
 
 # Get all groups that the user is a member of
-$groups = Get-ADPrincipalGroupMembership $user
+$groups = Get-ADPrincipalGroupMembership $user -ResourceContextServer $resource
 
 # Display the groups
 Write-Host "Groups for user '$username':" -ForegroundColor Green
