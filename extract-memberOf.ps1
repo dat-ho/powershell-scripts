310a498e82eab56e1307d03c5fb73c140082c688
Dat's Github <50114858+TienDatHo@users.noreply.github.com>
2023-05-01 10:38:40 +0800

    Update extract-memberOf.ps1

--- extract-memberOf.ps1
+++ extract-memberOf.ps1
@@ -1,3 +1,14 @@
-﻿Import-Module activedirectory
-$username = Read-Host 'Please enter Username:'
-Get-ADPrincipalGroupMembership $username -ResourceContextServer "rxxi.com.au" | Get-ADGroup -Properties * | select Name, Description, GroupScope | Sort-Object -Property Name | Format-Table -AutoSize
+# Enter the username of the user whose groups you want to extract
+$username = Read-Host "Please enter Username:"
+
+# Get the user object from Active Directory
+$user = Get-ADUser $username
+
+# Get all groups that the user is a member of
+$groups = Get-ADPrincipalGroupMembership $user
+
+# Display the groups
+Write-Host "Groups for user '$username':" -ForegroundColor Green
+foreach ($group in $groups) {
+    Write-Host "- $($group.Name)"
+}
