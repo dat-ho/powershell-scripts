572b62c8027f89c243c08d5472bd25f1c366269b
Tien Dat HO <50114858+TienDatHo@users.noreply.github.com>
2023-05-01 10:27:28 +0800

    Update extract-memberOf.ps1

--- extract-memberOf.ps1
+++ extract-memberOf.ps1
@@ -1,3 +1,3 @@
 ﻿Import-Module activedirectory
 $username = Read-Host 'Please enter Username:'
-Get-ADPrincipalGroupMembership $username -ResourceContextServer "rsmi.com.au" | Get-ADGroup -Properties * | select Name, Description, GroupScope | Sort-Object -Property Name | Format-Table -AutoSize
+Get-ADPrincipalGroupMembership $username -ResourceContextServer "rxxi.com.au" | Get-ADGroup -Properties * | select Name, Description, GroupScope | Sort-Object -Property Name | Format-Table -AutoSize
