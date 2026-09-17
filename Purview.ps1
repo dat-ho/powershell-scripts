8df09319f7365e9c4a56328caca13287ef7eea3c
Dat H. <50114858+dat-ho@users.noreply.github.com>
2026-09-17 11:53:12 +0800

    Update Purview.ps1

--- Purview.ps1
+++ Purview.ps1
@@ -1,1 +1,7 @@
+Connect-IPPSSession
 
+#Label Publishing Policy Settings
+(Get-LabelPolicy -Identity "Standard").Settings
+
+#Set label custom help page
+Set-LabelPolicy -Identity "IP-LabelPublishing-ForTesting-B" -AdvancedSettings @{customhelpurl="https://racwa.sharepoint.com/"}
